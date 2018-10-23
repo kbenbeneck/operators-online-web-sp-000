8f1faa6b3b55f060018b027385b89ad0f037cf1e
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-23 01:41:32 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,11 @@
 def unsafe?(speed)
-  if speed > 60 ? return true : return false
-  else speed < 40 ? return true : return false
-  end
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return false
+    
 end
 
 
